1df12ffd618fac40bbab0bc0279145d14133cb89
deepa-kini-lux <deepa.kini@luxeveryday.com>
2022-02-03 16:34:18 +1100

    POST request implemented

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-// returns a function
+const Joi = require('joi');
 const express = require('express');
 
-//returns an object with useful methods
-// like put(), get(), post(), delete()
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 2000
 
 const courses = [
@@ -23,6 +22,22 @@ app.get('/api/courses', (req, res) => {
 app.get('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
   if (!course) res.status(404).send('The course does not exist.')
+  res.send(course);
+});
+
+app.post('/api/courses', (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required()
+  });
+  const result = schema.validate(req.body);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+  }
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+  courses.push(course);
   res.send(course);
 });
 
