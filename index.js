4b21fb84f0d344715686c00fb588b30b4955ea82
deepa-kini-lux <deepa.kini@luxeveryday.com>
2022-02-03 15:00:01 +1100

    Using request parameters in Express methods

--- index.js
+++ index.js
@@ -6,16 +6,24 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 2000
 
+const courses = [
+  { id: 1, name: 'Course1' },
+  { id: 2, name: 'Course2' },
+  { id: 3, name: 'Course3' },
+]
+
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
 app.get('/api/courses', (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
 });
 
 app.get('/api/courses/:id', (req, res) => {
-  res.send(req.params.id);
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('The course does not exist.')
+  res.send(course);
 });
 
 app.listen(port, () => {
