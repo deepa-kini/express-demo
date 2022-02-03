79cd2188d26433e900a3aa8251866b09ac798f11
deepa-kini-lux <deepa.kini@luxeveryday.com>
2022-02-03 14:49:34 +1100

    Implement route to explicate how to use route parameters

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ app.get('/', (req, res) => {
 
 app.get('/api/courses', (req, res) => {
   res.send([1, 2, 3]);
-})
+});
+
+app.get('/api/courses/:id', (req, res) => {
+  res.send(req.params.id);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
